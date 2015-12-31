b8b52c585db0e1a25a5ab2eb673f86881ad3c73d
Khalid Omar Ali <khalid.ali@consultant.com>
2015-12-31 02:37:24 +0000

    refresh on exercise 4

--- ex4.clj
+++ ex4.clj
@@ -1,13 +1,7 @@
 (def height-in-inches (* 6 12))
-
-(def height-in-cm (* height-in-inches 2.54))
-
-(def total-height-in-cm (+ 175 158))
-
-(def avg-height-in-cm (/ total-height-in-cm 2))
-
-(def double-avg-height-in-cm (bigdec avg-height-in-cm))
-
-(def convert-avg-height-to-inches (/ avg-height-in-cm 2.54))
-
-(def covert-avg-height-to-feet (/ convert-avg-height-to-inches 12))
+(def height-in-inches (* 6 12))
+(def height-from-inches-to-centimetres (* height-in-inches 2.54))
+(def height-person1 174)
+(def height-person2 192)
+(def total-height-in-cm (+ height-from-inches-to-centimetres height-person1 height-person2))
+(def avg-height-in-cm (/ total-height-in-cm 3))
