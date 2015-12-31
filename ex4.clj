0fd4c5bf50e6711a1652744de75d24361272416c
Khalid Omar Ali <khalid.ali@consultant.com>
2015-12-31 02:46:10 +0000

    finish  arithmetic operations and start functions

--- ex4.clj
+++ ex4.clj
@@ -1,3 +1,4 @@
+; calculate average height of three people in cm
 (def height-in-inches (* 6 12))
 (def height-in-inches (* 6 12))
 (def height-from-inches-to-centimetres (* height-in-inches 2.54))
@@ -5,3 +6,7 @@
 (def height-person2 192)
 (def total-height-in-cm (+ height-from-inches-to-centimetres height-person1 height-person2))
 (def avg-height-in-cm (/ total-height-in-cm 3))
+; convert back to inches
+(def convert-height-back-to-inches (/ avg-height-in-cm 2.54))
+; convert back to feet
+(def convert-height-back-to-feet (/ convert-back-to-feet 12))
--- functions.clj
+++ functions.clj
@@ -1,29 +1,3 @@
-(defn forward-right
-  "Moves specified turtle forward and tilts its head"
-  [turtle]
-  (forward turtle 60)
-  (right turtle 135))
-(forward-right :trinity)
 
-(defn forward-right-with-len-ang 
-; description
-"Moves specified turtle forward and tilts its head"
-; arguments
-[turtle, len, angle]
-(forward turtle 60)
-(right turtle 135)
-)
-; call the function
-
-(defn forward-right-with-len-ang 
-; description
-"Moves specified turtle forward and tilts its head"
-; arguments
-[turtle, len, angle]
-(forward turtle 60)
-(right turtle 135)
-)
-
-(forward-right-with-len-ang :trinity 60 120)
 
 
