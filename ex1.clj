d96fa5e0b983abfe840b583a72e31fb1e98e563c
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-07 19:23:34 +0000

    restart learning clojure and set up a new dev environment with Spacemacs

--- ex1.clj
+++ ex1.clj
@@ -1,6 +1,6 @@
 (print-str "Hello, World")
 (print-str "Hello World!" " " "from Clojure")
-(+ 3 4)
+(4)
 (- 3 4)
 (* 3 4)
 
--- ex2.clj
+++ ex2.clj
@@ -3,4 +3,5 @@
 ; Type (forward 40) and evaluate this line by hitting Cmd + Enter or Ctrl + Enter (no shift key)
 ; See what happens
 ; Type (right 90) or other commands and evaluate the lines one by one by hitting Cmd + Enter or Ctrl + Enter (no shift key)
-; Take a look Turtles App API and How To Walk Turtles [section 1 and 2], and try more commands to walk your turtle
+;Take a look Turtles App API and How To Walk Turtles [section 1 and 2], and try more commands to walk your turtle
+; did this on spacemacs using Cider, you get REPL on the fly
