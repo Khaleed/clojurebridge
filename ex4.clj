ccf02450c10e1c4057395d1c20186b23813117e5
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-07 21:35:43 +0000

    revise simple values

--- ex4.clj
+++ ex4.clj
@@ -1,12 +1,11 @@
-; calculate average height of three people in cm
-(def height-in-inches (* 6 12))
+;calculate average height of three people in cm
 (def height-in-inches (* 6 12))
 (def height-from-inches-to-centimetres (* height-in-inches 2.54))
 (def height-person1 174)
 (def height-person2 192)
 (def total-height-in-cm (+ height-from-inches-to-centimetres height-person1 height-person2))
 (def avg-height-in-cm (/ total-height-in-cm 3))
-; convert back to inches
+;convert back to inches
 (def convert-height-back-to-inches (quot avg-height-in-cm 2.54))
-; convert back to feet
+;convert back to feet
 (def convert-height-back-to-feet (quot convert-back-to-feet 12))
