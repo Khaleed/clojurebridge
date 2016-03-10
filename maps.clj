08da9d9082c6ed7b2bac003a41a8d49634e896eb
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-10 23:21:59 +0000

    finish covering collections

--- maps.clj
+++ maps.clj
@@ -2,3 +2,7 @@
 ; learning about maps
 (def me {:first "Khalid" :middle "Omar" :last "Ali"})
 (merge me {:hometown "London"} {:sports "martial arts"})
+; exercise model classmates
+(def classmates [{:first "Joe" :last "Bloggs" :hometown "NYC"}{:first "Jane" :last "Doe"  :hometown "Boston"}{:first "Malcolm" :last "X" :hometown "Jamaica"}])
+; add your map to classmates vector
+(conj classmates me)
