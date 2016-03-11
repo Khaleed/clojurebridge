98f55e74c15ac9e09f4e392f3f83f9d03a805584
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-11 23:15:09 +0000

    define functions and call them with single and multiple arguments

--- functions.clj
+++ functions.clj
@@ -1,3 +1,13 @@
-
-
-
+; define forward-right function
+(defn forward-right
+  [turtle]
+  (forward turtle 60)
+  (right turtle 135))
+(forward-right :neo) ;=> {:neo {:angle 135}}
+; define function with multiple args
+(defn forward-right-with-len
+; multiple args
+  [turtle len]
+  (forward turtle len)
+  (right turtle 135))
+(forward-right-with-len :neo 90) ;=> {:trinity {:angle 90}}
