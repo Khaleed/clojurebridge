3d09c45abf2297a767aae0ba44d6c98bfb765176
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-19 17:29:19 +0000

    finished bonus exercise on higher order functions

--- high-order-funcs.clj
+++ high-order-funcs.clj
@@ -1,3 +1,15 @@
 ; map
 (map name (turtle-names)) ;=> ("trini" "neo" "oracle" "cypher")
 (map (partial + 90) [0 30 60 90]) ;=> (90 120 150 180)
+; reduce
+; exercise: create a function that takes a vector of maps
+
+; calculate avg
+(def data [{:angle 30} {:angle 90} {:angle 50}])
+
+(defn avg
+  [coll]
+  (/ (reduce + (map :angle coll))
+     (count coll)))
+
+(avg [{:angle 30} {:angle 90} {:angle 50}])
