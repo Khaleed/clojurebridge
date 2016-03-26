2966fca6642c8e1242e41941fe59bdd099b3b004
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-26 21:42:35 +0000

    finish with conditionals

--- control-flow.clj
+++ control-flow.clj
@@ -12,3 +12,14 @@
       (> (+ y 40) -150)
       -150
       :else (+ y 40))
+;exercise with conditionals
+(defn y-within-frame-cond
+  [y]
+  (cond (< (+ y 40) -150)
+        150
+        (> (+ y 40) 150)
+        -150
+        :else (+ y 40)))
+(y-within-frame-cond 200)
+(y-within-frame-cond -200)
+(y-within-frame-cond 0)
